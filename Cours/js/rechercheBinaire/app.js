483f1cdec3de7457925aa3c80126eaa2940c54b1
Taekyon <lean.asai@protonmail.com>
2022-10-14 12:02:45 +0200

    Rework on rBinaire

--- Cours/js/rechercheBinaire/app.js
+++ Cours/js/rechercheBinaire/app.js
@@ -1,28 +1,30 @@
-// import d'une moviesRenamed, qu'on a trier par ordre alphabétique.
+// import d'une var moviesRenamed, qu'on a trier par ordre alphabétique.
 /* ALT
  * const moviesRenamed = require("./moviesRenamed/movies"); */
-import moviesRenamed from "./movies.js";
+// import moviesRenamed from "./movies.js";
+const moviesDummies = ["Schindler's List", "Il buono, il brutto, il cattivo", "Fight Club", "Forrest Gump", "Inception", "One Flew Over the Cuckoo's Nest", "Goodfellas", "Shichinin no samurai", "Se7en", "La vita è bella"];
 
-console.log(moviesRenamed[13], moviesRenamed[15], moviesRenamed[17]); // Test
+const moviesTotal = moviesDummies.length;
+console.log(`${moviesDummies[0]}, ${moviesDummies[4]}, ${moviesDummies[moviesTotal - 1]}.
+nombre de films: ${moviesTotal}.`); // Test
 document.querySelector("movies-List");
-const moviesList = movies;
 
 // main app
-const binarySearch = (moviesRenamed, thingToFind, start, end) => {
+const binarySearch = (moviesDummies, thingToFind, start, end) => {
     if (start > end) {
         return false;
     }
     
     let mid = Math.floor((start + end) / 2);
-    if (moviesRenamed[mid] === thingToFind) {
+    if (moviesDummies[mid] === thingToFind) {
         return true;
     }
     
-    if (thingToFind < moviesRenamed[mid]) {
-        return binarySearch(moviesRenamed, thingToFind, start, mid - 1);
+    if (thingToFind < moviesDummies[mid]) {
+        return binarySearch(moviesDummies, thingToFind, start, mid - 1);
     
     } else {
     
-        return binarySearch(moviesRenamed, thingToFind, mid + 1, end);
+        return binarySearch(moviesDummies, thingToFind, mid + 1, end);
     }
 }
