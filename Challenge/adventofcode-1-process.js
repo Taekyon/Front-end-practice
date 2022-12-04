1ac00ac92d6b839aa2575652ac6df49eec7807a8
Taekyon <lean.asai@protonmail.com>
2022-12-04 17:51:01 +0100

    adventofcode day 1

---
+++ Challenge/adventofcode-1-process.js
@@ -0,0 +1,59 @@
+/**
+* Question générale :
+*   - comment prendre l'input dans le code ? plaintext to JSON ?
+*       - possible de l'intégrer dans le .js directement ? ou trop de donnée ?
+*
+* Je dois faire comprendre à js que chaque groupe est séparé par un espace.
+*   - créer un number et intégrer la ligne au nombre (avec +=).
+*   - terminer l'objet si *ligne nulle*.
+*   - *condition pour stop*.
+*
+*
+* CODE ALTERNATIF:
+* créer un objet par groupe, puis additionner chaque nombre dans cette objet.
+**/
+
+let inputTest = {
+2991
+13880
+13279
+1514
+9507
+
+6544
+9672
+13044
+4794
+6648
+8669
+
+2790
+1196
+3619
+1692
+8727
+2342
+1099
+6083
+3834
+2008
+
+2974
+4393
+1146
+4240
+1880
+3226
+2390
+5640
+2768
+1887
+4217
+3314
+5653
+2609
+};
+
+function parseFormat(strArg) {
+    
+}
